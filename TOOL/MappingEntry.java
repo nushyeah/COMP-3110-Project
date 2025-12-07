cc7e88e4bf11f815cde9844348aaa8a894e0ce1a
Zgurmani4 <gurmaniaman@gmail.com>
2025-12-07 00:10:01 -0500

    Add MappingEntry class for line mapping

--- TOOL/MappingEntry.java
+++ TOOL/MappingEntry.java
@@ -1,1 +1,12 @@
+public class MappingEntry {
+    public final int oldLine;   // -1 if added
+    public final int newLine;   // -1 if deleted
+    public final String status; // "unchanged", "added", "deleted", "modified(minor)" etc.
+
+    public MappingEntry(int oldLine, int newLine, String status) {
+        this.oldLine = oldLine;
+        this.newLine = newLine;
+        this.status = status;
+    }
+}
 
